9c0c45827965af0be345ab920b298553aca57267
Kevin Guan <KevinGuan.gm@gmail.com>
2016-05-23 08:19:18 +0800

    Fixed incorrect JSON format and variable name

--- diff_checker.py
+++ diff_checker.py
@@ -1,33 +1,33 @@
 #!/usr/bin/env python3
 from collections import OrderedDict
-from flask import Blueprint, jsonify, request
+from flask import Flask, jsonify, request
 
-diff_checker = Blueprint("diff_checker", __name__)
+diff_checker = Flask(__name__)
 
 
-def result_checker(database_name, balls, times, date):
+def result_checker(database, balls, times, date):
     rows = []
-    result = []
+    result = {'nodes': []}
 
-    with open(database_name, 'r') as f:
+    with open(database, 'r') as f:
         for line in f:
             l = line.rstrip().split('|')
             rows.append((l[0], set(l[1].split())))
 
-    database = OrderedDict(rows)
+    database_dict = OrderedDict(rows)
 
-    for sub_date, sub_balls in database.items():
+    for sub_date, sub_balls in database_dict.items():
         if sub_date == date:
             continue
 
-        duplicates = balls.intersection(sub_balls)
+        duplicates = set(balls).intersection(set(sub_balls))
 
         if len(duplicates) >= times:
-            result.append({
+            result['nodes'].append({
                 'date': sub_date,
-                'balls': sub_balls,
+                'balls': list(sub_balls),
                 'times': len(duplicates),
-                'duplicates': duplicates
+                'duplicates': list(duplicates)
             })
 
     return result
@@ -38,3 +38,5 @@ def request_receiver():
     # get and load the data as json, then run `result_checker` on the data,
     # and send it as json use `jsonify` function
     return jsonify(result_checker(**request.json))
+
+diff_checker.run(port=80, debug=True)
