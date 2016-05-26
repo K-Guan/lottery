9673fb30de8c0b61566fbfd0043d8610b9dc737e
Kevin Guan <KevinGuan.gm@gmail.com>
2016-05-26 08:46:00 +0800

    Updated database and added database of 七乐彩

--- data_downloader.py
+++ data_downloader.py
@@ -1,30 +1,31 @@
 #!/usr/bin/env python3
+import sys
 import requests
 from bs4 import BeautifulSoup
-from collections import OrderedDict
 
-link = 'http://kaijiang.zhcw.com/zhcw/html/ssq/list_1.html'
-
-with open('ssq') as f:
-    last_date = f.readline().split('|')[0]
-
-
-result = OrderedDict()
-result_string = ''
+database = sys.argv[1]
+link = 'http://kaijiang.zhcw.com/zhcw/html/{}/list_1.html'.format(database)
 
 soup = BeautifulSoup(requests.get(link).text, "html.parser")
-elements = soup.find_all('tr')[2:-1]
+rows = soup.find_all('tr')[2:-1]
 
-for element in elements:
-    date = element.find_all('td')[1].text
-    result[date] = [i.text for i in element.find_all('em')][:-1]
+with open(database) as f:
+    last_date = int(f.readline().split('|')[0])
 
-
-for date, balls in result.items():
-    if int(date) > int(last_date):
-        result_string += '|'.join((date, ' '.join(balls))) + '\n'
-
-with open('ssq', 'r+') as f:
+with open(database, 'r+') as f:
     content = f.read()
     f.seek(0, 0)
-    f.write(result_string + content)
+
+    for row in rows:
+        date, balls = [i.text for i in row.find_all('td')[1:3]]
+
+        if int(date) > last_date:
+            f.write('|'.join([date, ' '.join(sorted(balls.strip().split()[:-1]
+                                                    if len(sys.argv) > 2
+                                                    else balls.strip().split(),
+                                                    key=int))]))
+            f.write('\n')
+        else:
+            break
+
+    f.write(content)
